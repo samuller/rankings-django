0f10466c4b27a06c46eb652be1ff0d52d9462eda
Simon Müller <samullers@gmail.com>
2016-10-15 22:34:31 +0200

    Add most previous URLs

--- rankings/previous/urls.py
+++ rankings/previous/urls.py
@@ -3,5 +3,16 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
+    url(r'^$', views.main_page),
+    url(r'^about$', views.about, name='about'),
+    url(r'^api/validate_all$', views.validate_all_matches),
+    url(r'^(?P<activity_url>.+)/$', views.activity_summary, name='activity_summary'),
+    url(r'^(?P<activity_url>.+)/players$', views.list_players),
+    url(r'^(?P<activity_url>.+)/player/(?P<player_id>[0-9]+)$', views.player_info),
+    url(r'^(?P<activity_url>.+)/player/(?P<player_id>[0-9]+)/history$', views.player_history),
+    url(r'^(?P<activity_url>.+)/matches$', views.list_matches),
+    url(r'^(?P<activity_url>.+)/update$', views.update),
+    url(r'^(?P<activity_url>.+)/api/get_players$', views.get_players),
+    url(r'^(?P<activity_url>.+)/api/validate_match$', views.validate_match),
+    url(r'^(?P<activity_url>.+)/api/add_matches$', views.submit_match),
+    ]
--- rankings/previous/views.py
+++ rankings/previous/views.py
@@ -3,6 +3,54 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
-def index(request):
+
+
+def main_page(request):
+    context = {}
+    return render(request, 'main_page.html', context)
+
+
+def validate_all_matches(request):
+    return HttpResponse("")
+
+
+def activity_summary(request, activity_url):
+    return HttpResponse("")
+
+
+def list_players(request, activity_url):
+    return HttpResponse("")
+
+
+def player_info(request, activity_url, player_id):
+    return HttpResponse("")
+
+
+def player_history(request, activity_url, player_id):
+    return HttpResponse("")
+
+
+def list_matches(request, activity_url):
+    return HttpResponse("")
+
+
+def update(request, activity_url):
+    return HttpResponse("")
+
+
+def get_players(request, activity_url):
+    return HttpResponse("")
+
+
+def validate_match(request, activity_url):
+    return HttpResponse("")
+
+
+def submit_match(request, activity_url):
+    return HttpResponse("")
+
+
+def about(request):
     context = {}
     return render(request, 'about.html', context)
+
