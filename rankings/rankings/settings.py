52296f5849a8a0922e6e00fd35b1ddd61e1795c6
Simon Müller <samullers@gmail.com>
2017-05-26 12:27:01 +0200

    Add massadmin for bulk editing features in admin interface

--- rankings/rankings/settings.py
+++ rankings/rankings/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'massadmin',
     'matches.apps.MatchesConfig',
     'previous.apps.PreviousConfig'
 ]
--- rankings/rankings/urls.py
+++ rankings/rankings/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include("massadmin.urls")),
     url(r'^', include('previous.urls')),
 ]
