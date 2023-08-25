0a87d283cff06bdfb8c1db09969d9a3e3419a08f
Simon Muller <samullers@gmail.com>
2023-08-25 17:39:11 +0200

    Add run.sh script

---
+++ run.sh
@@ -0,0 +1,47 @@
+#
+# Script for running commonly used commands quickly, e.g. "./run.sh lint". See: "./run.sh help".
+#
+
+# Fail on first error.
+set -e
+
+help() {
+    echo "You have to provide a command to run, e.g. '$0 lint'"
+    # Get all function names and then add indent to each line.
+    commands=$(compgen -A function | sed 's/^/  /')
+    echo -e "All commands available are:\n$commands"
+    exit
+}
+
+lint() {
+    echo "flake8..."
+    poetry run flake8 --exclude **/*/migrations/
+    # We're not as strict about docstrings in our tests
+    # poetry run flake8 --extend-ignore=D tests/
+    echo "mypy..."
+    poetry run mypy .
+    # No "strict" type requirements for tests.
+    # poetry run mypy tests/
+    echo "black..."
+    poetry run black --check rankings/
+}
+
+format() {
+    black rankings/
+}
+
+test() {
+    cd rankings
+    poetry run coverage run --branch --source='.' manage.py test --debug-mode
+    poetry run coverage html
+    poetry run coverage report --fail-under=65
+}
+
+if [ "$#" -gt 1 ]; then
+    echo -n "Too many args. "
+    help
+fi
+
+# Run function with same name of CLI argument (default to "help").
+cmd=${1:-"help"}
+$cmd
