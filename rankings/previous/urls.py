3f5f7d0e23f3fa6cda13a64a9d20db314f86d4c4
Simon Muller <samullers@gmail.com>
2023-08-27 11:00:00 +0200

    Update to Django 3.1

--- rankings/previous/urls.py
+++ rankings/previous/urls.py
@@ -1,53 +1,53 @@
 """Django URL patterns to redirect URL paths to specific views."""
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.main_page, name="home"),
-    url(r"^about$", views.about, name="about"),
-    url(r"^api/id$", views.show_id),
-    url(r"^api/fix_player$", views.replace_player_in_submissions, name="fix_player"),
-    url(r"^api/validate_all$", views.validate_all_matches),
-    url(
+    re_path(r"^$", views.main_page, name="home"),
+    re_path(r"^about$", views.about, name="about"),
+    re_path(r"^api/id$", views.show_id),
+    re_path(r"^api/fix_player$", views.replace_player_in_submissions, name="fix_player"),
+    re_path(r"^api/validate_all$", views.validate_all_matches),
+    re_path(
         r"^select_player_to_fix/(?P<session_ids_str>.*)$",
         views.select_player_to_replace_in_submissions,
         name="select_fix_player",
     ),
-    url(r"^(?P<activity_url>[^/]+)/$", views.activity_summary, name="activity_summary"),
-    url(
+    re_path(r"^(?P<activity_url>[^/]+)/$", views.activity_summary, name="activity_summary"),
+    re_path(
         r"^(?P<activity_url>.+)/players/(?P<sort_by>.*)$",
         views.list_players,
         name="list_players",
     ),
-    url(
+    re_path(
         r"^(?P<activity_url>.+)/player/(?P<player_id>[0-9]+)$",
         views.player_info,
         name="player_info",
     ),
-    url(
+    re_path(
         r"^(?P<activity_url>.+)/player/(?P<player_id>[0-9]+)/history$",
         views.player_history,
     ),
-    url(r"^(?P<activity_url>.+)/matches$", views.list_matches, name="list_matches"),
-    url(
+    re_path(r"^(?P<activity_url>.+)/matches$", views.list_matches, name="list_matches"),
+    re_path(
         r"^(?P<activity_url>.+)/matches/(?P<page>[0-9]+)$",
         views.list_matches,
         name="list_matches",
     ),
-    url(r"^(?P<activity_url>.+)/match/$", views.list_matches, name="list_match"),
-    url(
+    re_path(r"^(?P<activity_url>.+)/match/$", views.list_matches, name="list_match"),
+    re_path(
         r"^(?P<activity_url>.+)/match/(?P<match_id>[0-9]+)$",
         views.list_matches,
         name="list_match",
     ),
-    url(r"^(?P<activity_url>.+)/update$", views.update, name="update_rankings"),
-    url(
+    re_path(r"^(?P<activity_url>.+)/update$", views.update, name="update_rankings"),
+    re_path(
         r"^(?P<activity_url>.+)/update/(?P<year>[0-9]+)$",
         views.update,
         name="update_rankings",
     ),
-    url(r"^(?P<activity_url>.+)/api/get_players$", views.get_players),
-    url(r"^(?P<activity_url>.+)/api/add_matches$", views.submit_match),
-    url(r"^(?P<activity_url>.+)/api/undo_submission$", views.undo_submit),
+    re_path(r"^(?P<activity_url>.+)/api/get_players$", views.get_players),
+    re_path(r"^(?P<activity_url>.+)/api/add_matches$", views.submit_match),
+    re_path(r"^(?P<activity_url>.+)/api/undo_submission$", views.undo_submit),
 ]
--- rankings/rankings/urls.py
+++ rankings/rankings/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"^admin/", include("massadmin.urls")),
-    url(r"^", include("previous.urls")),
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^admin/", include("massadmin.urls")),
+    re_path(r"^", include("previous.urls")),
 ]
