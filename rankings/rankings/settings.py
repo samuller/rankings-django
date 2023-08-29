fb86a5582ca454f5c0d5336c8e45e4a59736abc4
Simon Muller <samullers@gmail.com>
2023-08-29 13:24:25 +0200

    Add django_filter library

--- rankings/rankings/settings.py
+++ rankings/rankings/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "massadmin",
+    'django_filters',
     "rest_framework",
     "previous.apps.PreviousConfig",
 ]
@@ -110,6 +111,7 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
     ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 
