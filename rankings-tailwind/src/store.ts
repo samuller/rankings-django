f6d5d92294bf1e35b601fee9391192666515f043
Simon Muller <samullers@gmail.com>
2023-09-01 20:45:48 +0200

    Add store for activities fetched from API

--- rankings-tailwind/src/store.ts
+++ rankings-tailwind/src/store.ts
@@ -1,4 +1,16 @@
 import { writable } from 'svelte/store';
+import { asyncReadable } from '@square/svelte-store';
 
 export const page_title = writable("");
+
+
+export const activities = asyncReadable(
+    [],
+    async () => {
+      const response = await fetch('/api/activities/?active=true&select=url,name');
+      const activityList = await response.json();
+      return activityList;
+    },
+    { reloadable: true }
+);
 
