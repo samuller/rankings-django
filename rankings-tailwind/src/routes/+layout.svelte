6dc463fd9a94a6c908b7a3898ee45180cf15e8e2
Simon Muller <samullers@gmail.com>
2023-09-01 22:45:53 +0200

    Center align main contents

--- rankings-tailwind/src/routes/+layout.svelte
+++ rankings-tailwind/src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 
 <div class="h-screen flex flex-col">
   <Navbar></Navbar>
-  <section class="bg-gray-300 flex flex-col justify-start gap-6 p-4 flex-1">
+  <section class="bg-gray-300 flex-1 flex flex-col justify-start items-center gap-6 p-4">
   <slot />
   </section>
   <Footer></Footer>
