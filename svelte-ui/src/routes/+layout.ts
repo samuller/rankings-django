6da106f5f9989666abb2d677cb952ab42af0cbdc
Simon Muller <samullers@gmail.com>
2023-09-03 23:34:12 +0200

    Disable SSR to fix undefined variables in browser

--- svelte-ui/src/routes/+layout.ts
+++ svelte-ui/src/routes/+layout.ts
@@ -1,2 +1,5 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
 export const prerender = true;
+
+// Decalare SvelteKit app to be a fully client-rendered single-page app (SPA).
+export const ssr = false;
