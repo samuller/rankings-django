d3d4cfc7a533e444698c1a4073aa94b55dd6c140
Simon Muller <samullers@gmail.com>
2023-09-03 23:33:30 +0200

    Create helper function for API stores

--- svelte-ui/src/store.ts
+++ svelte-ui/src/store.ts
@@ -1,18 +1,24 @@
+import { browser } from "$app/environment";
 import { writable } from 'svelte/store';
-import { asyncReadable } from '@square/svelte-store';
+import { asyncReadable, type Loadable } from '@square/svelte-store';
+
+
+const readJSONAPI = function(initial: any, url: string): Loadable<any> {
+  if (!browser) return {} as Loadable<any>;
+  return asyncReadable(
+      initial,
+      async () => {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw { message: response.statusText, status: response.status }
+        }
+        const jsonData = await response.json();
+        return jsonData;
+      },
+      { reloadable: true }
+  );
+}
 
 export const page_title = writable("");
 
-export const activities = asyncReadable(
-    [],
-    async () => {
-      const response = await fetch('/api/activities/?active=true&select=url,name');
-      if (!response.ok) {
-        throw { message: response.statusText, status: response.status }
-      }
-      const activityList = await response.json();
-      return activityList;
-    },
-    { reloadable: true }
-);
-
+export const activities = readJSONAPI([], '/api/activities/?active=true&select=url,name')
