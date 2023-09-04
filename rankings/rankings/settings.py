205f5e2e8a8842814fae66bf5d23edfe8f726e75
Simon Muller <samullers@gmail.com>
2023-09-04 19:34:15 +0200

    Default APIs to have 'ordering' param

--- rankings/rankings/settings.py
+++ rankings/rankings/settings.py
@@ -114,6 +114,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
     ],
     "DEFAULT_PAGINATION_CLASS": "drf_link_header_pagination.LinkHeaderLimitOffsetPagination",
     "PAGE_SIZE": 100,
