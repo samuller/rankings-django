420fec4113cc76ff81eaea15f85331ba0429b471
Simon Muller <samullers@gmail.com>
2023-09-04 21:52:22 +0200

    Update table to use data from props

--- svelte-ui/src/lib/table.svelte
+++ svelte-ui/src/lib/table.svelte
@@ -1,40 +1,33 @@
+<script lang="ts">
+    export let columnNames: string[];
+    export let rows: string[][];
+
+    $: columnAlignments = columnNames.map((val, idx) => {
+        return idx == columnNames.length - 1 ? "text-right" : "text-left"
+    });
+</script>
+
 <div class="relative overflow-x-auto">
-    <table class="w-full table-fixed sm:w-96 mx-auto text-sm text-left text-gray-500 dark:text-gray-400">
+    <table class="w-full table-auto sm:w-96 mx-auto text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3">
-                    Name
+                {#each columnNames as name, idx}
+                <th scope="col" class="px-6 py-3 {columnAlignments[idx]}">
+                    {name}
                 </th>
-                <th scope="col" class="px-6 py-3 text-right">
-                    Skill
-                </th>
+                {/each}
             </tr>
         </thead>
         <tbody>
+            {#each rows as row, row_idx}
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4 text-right">
-                    $2999
+                {#each row as cell, cell_idx}
+                <td class="px-6 py-4 text-white {columnAlignments[cell_idx]}">
+                    {cell}
                 </td>
+                {/each}
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro Delux XXYS Super Package
-                </th>
-                <td class="px-6 py-4 text-right">
-                    $1999
-                </td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td class="px-6 py-4 text-right">
-                    $99
-                </td>
-            </tr>
+            {/each}
         </tbody>
     </table>
 </div>
