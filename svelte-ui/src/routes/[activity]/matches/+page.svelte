abd337b6529805ef9aa85d873d96c4d1f9386a9a
Simon Muller <samullers@gmail.com>
2023-09-08 20:43:49 +0200

    Add route that lists matches in activity

---
+++ svelte-ui/src/routes/[activity]/matches/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import type { ActivityPage } from './+page';
+	import { AddButton, Card, DynamicData, Table, type RowDetail } from '$lib/components';
+	import {
+		currentActivityUrl,
+		currentActivity,
+		navTitle,
+		activities,
+		type Matches,
+		apiMatches,
+	} from '../../../store';
+
+	export let data: ActivityPage;
+
+	let matchesTable: RowDetail[][] = [];
+	currentActivityUrl.set(data.activity_url);
+
+	$: if ($currentActivity) { navTitle.set($currentActivity.name); }
+	$: matches = apiMatches[data.activity_url];
+	$: matchesTable = $matches
+		.map((match: Matches) => [
+			{ text: "ID" },
+			{ text: new Date(match.datetime*1000).toISOString().replace('T', ' ').split('.')[0]+"Z" },
+			{ text: match.teams[0].members.map((member) => member.player.name).join(" & ") },
+			{ text: match.teams[1].members.map((member) => member.player.name).join(" & ") },
+		]);
+</script>
+
+<svelte:head>
+{#if $currentActivity}
+	<title>Rankings - {$navTitle}</title>
+{:else}
+	<title>Rankings</title>
+{/if}
+</svelte:head>
+
+<DynamicData data={activities}></DynamicData>
+
+{#if $currentActivity}
+	<Card class="w-1/2 2xl:w-[calc(0.5*1536px)] flex justify-center" style="tight">
+		<span class="normal-case text-xl font-semibold">{$currentActivity.name}</span>
+	</Card>
+
+	<div class="w-1/2 text-gray-700">
+		<h2 class="text-2xl font-bold text-left">Match history</h2>
+		<p class="text-left">Reported matches which have been validated.</p>
+	</div>
+	<DynamicData data={matches}></DynamicData>
+	{#if matchesTable.length > 0}
+		<Table
+			columnNames={['ID #', 'Date', 'Team 1', 'Team 2']}
+			columnAlignments={['text-center', 'text-center', 'text-center', 'text-center']}
+			rows={matchesTable}
+		></Table>
+	{/if}
+{/if}
