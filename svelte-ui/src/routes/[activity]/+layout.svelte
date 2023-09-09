090115e91ebd10d7f44329541628815de40a5297
Simon Muller <samullers@gmail.com>
2023-09-09 17:31:30 +0200

    Disable double header when player profile

--- svelte-ui/src/routes/[activity]/+layout.svelte
+++ svelte-ui/src/routes/[activity]/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { page } from '$app/stores';
 	import type { ActivityPage } from './+page';
 	import { Card, DynamicData } from '$lib/components';
 	import {
@@ -11,6 +12,7 @@
 	export let data: ActivityPage;
 	currentActivityUrl.set(data.activity_url);
 	$: if ($currentActivity) { navTitle.set($currentActivity.name); }
+    $: viewingPlayerProfile = $page.url.pathname.includes("/player/");
 </script>
 
 <svelte:head>
@@ -24,9 +26,11 @@
 <DynamicData data={activities}></DynamicData>
 
 {#if $currentActivity}
-    <Card class="w-full md:w-1/2 2xl:w-[calc(0.5*1536px)] flex justify-center" style="tight">
-        <span class="normal-case text-xl font-semibold">{$currentActivity.name}</span>
-	</Card>
+    {#if !viewingPlayerProfile}
+        <Card class="w-full md:w-1/2 2xl:w-[calc(0.5*1536px)] flex justify-center" style="tight">
+            <span class="normal-case text-xl font-semibold">{$currentActivity.name}</span>
+        </Card>
+    {/if}
 
 	<slot />
 {/if}
