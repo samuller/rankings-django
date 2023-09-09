c3d48b27b0c9a242d5a9f951cd47d331531a3e89
Simon Muller <samullers@gmail.com>
2023-09-09 15:07:09 +0200

    Disable pagination buttons instead of hiding

--- svelte-ui/src/lib/components/paging-nav.svelte
+++ svelte-ui/src/lib/components/paging-nav.svelte
@@ -1,38 +1,49 @@
 <script lang="ts">
-    import type { PageableAPIStore } from '../../store';
+	import type { PageableAPIStore } from '../../store';
 
-    export let pageNr = 1;
-    export let pageable: PageableAPIStore<any>;
+	export let pageNr = 1;
+	export let pageable: PageableAPIStore<any>;
 
-    function setQuery(key: string, value: string) {
+	function setQuery(key: string, value: string) {
 		const currURL = new URL(window.location.href);
 		currURL.searchParams.set(key, value);
 		return currURL;
 	}
+
+	$: showFirst = pageable.paged() && pageable.firstURL() != null && pageable.prevURL() != null;
+	$: showPrev = pageable.paged() && pageable.prevURL() != null;
+	$: showNext = pageable.paged() && pageable.nextURL() != null;
+	$: showLast = pageable.paged() && pageable.lastURL() != null && pageable.nextURL() != null;
 </script>
 
-
 <div class="join">
-    {#if pageable.paged() && pageable.firstURL() && pageable.prevURL()}
-    <div class="tooltip" data-tip="First">
-        <a href={setQuery('page', '1').href}  class="join-item btn">&lt;&lt;</a>
-    </div>
-    {/if}
-    {#if pageable.paged() && pageable.prevURL()}
-    <div class="tooltip" data-tip="Previous">
-        <a href={setQuery('page', (pageNr - 1).toString()).href}  class="join-item btn">&lt;</a>
-    </div>
-    {/if}
-    {#if pageable.paged() && pageable.nextURL()}
-    <div class="tooltip" data-tip="Next">
-        <a href={setQuery('page', (pageNr + 1).toString()).href} class="join-item btn">&gt;</a>
-    </div>
-    {/if}
-    {#if pageable.paged() && pageable.lastURL() && pageable.nextURL()}
-    <div class="tooltip" data-tip="last">
-        <a
-            href={setQuery('page', pageable.pageFromURL(pageable.lastURL() ?? '').toString()).href}
-            class="join-item btn normal-case">&gt;&gt;</a>
-    </div>
-    {/if}
+	<div class="tooltip" data-tip="First">
+		<a
+			href={setQuery('page', '1').href}
+			class="join-item btn text-xl"
+			class:btn-disabled={!showFirst}>«</a
+		>
+	</div>
+	<div class="tooltip" data-tip="Previous">
+		<a
+			href={setQuery('page', (pageNr - 1).toString()).href}
+			class="join-item btn btn-disabled text-xl"
+			class:btn-disabled={!showPrev}>﹤</a
+		>
+	</div>
+	<button class="join-item btn normal-case">Page {pageNr}</button>
+	<div class="tooltip" data-tip="Next">
+		<a
+			href={setQuery('page', (pageNr + 1).toString()).href}
+			class="join-item btn text-xl"
+			class:btn-disabled={!showNext}>﹥</a
+		>
+	</div>
+	<div class="tooltip" data-tip="Last">
+		<a
+			href={setQuery('page', pageable.pageFromURL(pageable.lastURL() ?? '').toString()).href}
+			class="join-item btn normal-case text-xl"
+			class:btn-disabled={!showLast}>»</a
+		>
+	</div>
 </div>
