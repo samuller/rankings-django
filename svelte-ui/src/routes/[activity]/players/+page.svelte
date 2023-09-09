2c656bf5960371200234f39e6f4c15cb90fd1e8c
Simon Muller <samullers@gmail.com>
2023-09-09 18:36:13 +0200

    Add listing of all active players

---
+++ svelte-ui/src/routes/[activity]/players/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import { DynamicData, Table, type RowDetail } from '$lib/components';
+	import {
+		type Ranking,
+		apiRankings,
+	} from '../../../store';
+
+	let rankingsTable: RowDetail[][] = [];
+
+	$: rankings = apiRankings[$page.params.activity];
+	$: rankingsTable = $rankings
+		.filter((ranking: Ranking) => ranking.skill > 0)
+		.map((ranking: Ranking) => [
+			{ text: ranking.player.name, url: `/${$page.params.activity}/player/${ranking.player.id}` },
+			{ text: ranking.skill.toFixed(0) }
+		]);
+</script>
+
+<DynamicData data={rankings}></DynamicData>
+{#if rankingsTable.length > 0}
+<div class="w-full md:w-1/2 text-gray-700">
+	<h2 class="text-2xl font-bold text-left">All players</h2>
+	<p class="text-left">The current rankings according to the validated match history.</p>
+</div>
+<Table
+	columnNames={['Name', 'Skill']}
+	columnAlignments={['text-left', 'text-right']}
+	rows={rankingsTable}
+></Table>
+{/if}
