558e2c7c2709ef2ca13523b9bd9289a8a5824eb0
Simon Muller <samullers@gmail.com>
2023-09-10 21:42:43 +0200

    Disable round-robin submit until inputs correct

--- svelte-ui/src/lib/components/add-match.svelte
+++ svelte-ui/src/lib/components/add-match.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
 	import Tabs from "./tabs.svelte";
 
+    interface Player {
+        name: string;
+        id: number;
+    }
+
     const teamNames = ["Team 1", "Team 2"];
     const defaultNames = [["Player 1", "Player 2"], ["Player 3", "Player 4"]];
     // Full list of all known possible players.
-    export let players: { name: string, id: number }[] = [];
+    export let players: Player[] = [];
 
     let selectedMemberIds = [[0, 0], [0, 0]];
+    $: selectedPlayers = selectedMemberIds.flat().filter((id) => id != 0).map((id) => {
+        return players.find((player) => player.id == id);
+    }) as Player[];
     $: validMemberSelection = selectedMemberIds[0][0] != 0 && selectedMemberIds[1][0] != 0;
 
+    /**
+     * State and functions for multiple match selection.
+     */
     let multiMatchList: HTMLSelectElement;
     let multiMatchWins: number[] = [];
 
@@ -17,6 +28,7 @@
         // Indicate variable has changed to trigger updates.
         multiMatchWins = multiMatchWins;
     };
+
     const removeSelectedMultiMatches = function () {
         // Loop backwards so that indexes aren't changed after each remove.
         for (let idx = multiMatchList.selectedOptions.length - 1; idx >= 0; idx--) {
@@ -25,12 +37,25 @@
         }
         multiMatchWins = multiMatchWins;
     };
+
+    /**
+     * State and functions for round-robin selection.
+     */
+    let roundRobinWinPlayer = 0;
+    let roundRobinLosePlayer = 0;
+    $: roundRobinSelected = roundRobinWinPlayer != 0 || roundRobinLosePlayer != 0;
+
+    const resetRoundRobinSelect = function() {
+        roundRobinWinPlayer = 0;
+        roundRobinLosePlayer = 0;
+    }
 </script>
 
 <h3 class="font-bold text-2xl">Match results</h3>
 <p class="pt-4">Specify the teams that played:</p>
 
 <div>
+    <!-- Select teams & players -->
     {#each teamNames as team, teamIdx}
     <table class="table">
         <thead>
@@ -43,6 +68,7 @@
               {#each defaultNames[teamIdx] as member, memberIdx}
               <select
                 bind:value={selectedMemberIds[teamIdx][memberIdx]}
+                on:change={() => resetRoundRobinSelect()}
                 class="flex-1 m-1 sm:m-3 select select-bordered sm:max-w-xs">
                   <option selected value={0}>[{member}]</option>
                   {#each players as player}
@@ -54,6 +80,8 @@
         </tbody>
     </table>
     {/each}
+
+    <!-- Multiple ways to select the winnig team or submit multiple match results. -->
     <Tabs data={[
         { title: "Single Match", id: "single-match" },
         { title: "Multiple Matches", id: "multi-match" },
@@ -103,21 +131,33 @@
             <div class="flex flex-col items-center">
                 <span class="flex flex-row items-baseline">
                     <p>Won 3 games:</p>
-                    <select class="flex-1 m-1 sm:m-3 select select-bordered sm:max-w-xs">
-                        <option selected>(None)</option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
+                    <select
+                      bind:value={roundRobinWinPlayer}
+                      disabled={roundRobinLosePlayer != 0}
+                        class="flex-1 m-1 sm:m-3 select select-bordered sm:max-w-xs">
+                        <option selected value={0}>(None)</option>
+                        {#each selectedPlayers as player}
+                        <option value={player.id}>{player.name}</option>
+                        {/each}
                     </select>
                 </span>
                 <span class="flex flex-row items-baseline">
                     <p>Lost 3 games:</p>
-                    <select class="flex-1 m-1 sm:m-3 select select-bordered sm:max-w-xs">
-                        <option selected>(None)</option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
+                    <select
+                      bind:value={roundRobinLosePlayer}
+                      disabled={roundRobinWinPlayer != 0}
+                      class="flex-1 m-1 sm:m-3 select select-bordered sm:max-w-xs">
+                        <option selected value={0}>(None)</option>
+                        {#each selectedPlayers as player}
+                        <option value={player.id}>{player.name}</option>
+                        {/each}
                     </select>
                 </span>
-                <button class="btn btn-primary flex-1 md:w-1/2">Submit</button>
+                <button
+                  class="btn btn-primary flex-1 md:w-1/2"
+                  disabled={selectedPlayers.length != 4 || !roundRobinSelected}>
+                    Submit
+                </button>
             </div>
         </div>
     </Tabs>
