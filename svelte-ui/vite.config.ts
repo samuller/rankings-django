a3dd2a3f8e7aefce8bf5492312ab0bd25a23f645
Simon Muller <samullers@gmail.com>
2023-09-12 20:13:52 +0200

    Include any URL containing /api/ in proxy

--- svelte-ui/vite.config.ts
+++ svelte-ui/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 	},
 	server: {
 		proxy: {
-		  "/api": {
+		  "^.*/api/": {
 			target: "http://host.docker.internal:8000",
 			changeOrigin: true,
 			secure: false,
