43565dc486e6f6e74e83b55525c8c1457732b95f
Simon Muller <samullers@gmail.com>
2023-09-14 20:34:30 +0200

    Add "about" to Activity API, but not by default

--- rankings/previous/api.py
+++ rankings/previous/api.py
@@ -66,8 +66,20 @@ class ActivitySerializer(
             "max_teams_per_match",
             "min_players_per_team",
             "max_players_per_team",
+            "about"
         ]
 
+    def get_fields(self):
+        """Customise the list of fields to serialize."""
+        fields = super().get_fields()
+        request = self.context.get('request')
+        if request:
+            # We remove the large "about" field by default, and only return it if you use the field_filter to
+            # selectively choose fields (one of which be "about").
+            field_filter = request.query_params.get(FIELD_FILTER_PARAM)
+            if not field_filter:
+                fields.pop("about")
+        return fields
 
 class ActivityViewSet(FieldFilterMixin, ValidateParamsMixin, viewsets.ModelViewSet):
     """ViewSet for viewing and editing Activities."""
