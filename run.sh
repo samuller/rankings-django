5e3611f267e7bd728c1668b4ed347a531804a38c
Simon Muller <samullers@gmail.com>
2023-10-07 20:53:42 +0200

    Docker: use UI version for image

--- run.sh
+++ run.sh
@@ -71,14 +71,15 @@ build() {
     # - https://docs.gitlab.com/ee/ci/docker/docker_layer_caching.html
     # - https://stackoverflow.com/questions/52646303/is-it-possible-to-cache-multi-stage-docker-builds/68459169#68459169
     # - https://docs.docker.com/engine/reference/commandline/build/#cache-from
-    IMAGE="docker.io/library/rankings-site-test"
-    VERSION=$(cat rankings/pyproject.toml | grep "^version = " | cut -d' ' -f3 | tr -d '"')
+    IMAGE="rankings-site-test"
+    API_VERSION=$(cat rankings/pyproject.toml | grep "^version = " | cut -d' ' -f3 | tr -d '"')
+    UI_VERSION=$(cat svelte-ui/package.json | grep '"version": "' | cut -d':' -f2 | tr -d ' ",')
     GIT_HASH=$(git rev-parse HEAD)
     gen-docker
     time docker build \
-        --label "org.opencontainers.image.version=$VERSION" \
+        --label "org.opencontainers.image.version=$UI_VERSION" \
         --label "org.opencontainers.image.revision=$GIT_HASH" \
-        --tag "$IMAGE:0.9.0" \
+        --tag "$IMAGE:$UI_VERSION" \
         --tag "$IMAGE:latest" \
         -f deploy/Dockerfile \
         .
