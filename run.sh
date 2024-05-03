f71f02375ad2d4a7528128a15f130abbc85839f1
Simon Muller <samullers@gmail.com>
2024-05-03 15:02:09 +0200

    Fix run.sh help command

--- run.sh
+++ run.sh
@@ -146,8 +146,16 @@ build() {
 # Find all declared functions that are not from exports (-fx). This will only pick up functions before this point.
 KNOWN_COMMANDS=$(declare -F | grep -v "\-fx")
 
+# If column command is not available, create a no-op function to replace it and prevent errors.
+# Alternatively, install it with: apt-get install -y bsdmainutils
+if ! type column >/dev/null 2>&1
+then function column { cat - ;}
+fi
+
 # Run function with same name of CLI argument (default to "help").
 cmd=${1:-"help"}
-# Remove argument we've already used.
-shift
+if [ "$#" -gt 0 ]; then
+    # Remove argument we've already used.
+    shift
+fi
 $cmd "$@"
