e0a513c7f6d78a9f631eeb6c90fbf066b64d9b83
Simon Muller <samullers@gmail.com>
2024-05-05 19:16:43 +0200

    Include UIDs in error message

--- rankings/rankings/check_db.py
+++ rankings/rankings/check_db.py
@@ -12,7 +12,10 @@ class CheckDBMiddleware:
     def __init__(self, get_response):
         db_path = Path(settings.DATABASES["default"]["NAME"])
         if db_path.exists() and not os.access(str(db_path), os.W_OK):
-            raise Exception(f"Database file is not writable: {str(db_path)}")
+            curr_uid = os.getuid()
+            file_uid = os.stat(db_path).st_uid
+            cmp_str = f"current vs file UIDs: {curr_uid} vs. {file_uid}"
+            raise Exception(f"Database file is not writable: {str(db_path)} ({cmp_str})")
         # Indicate that this middleware won't be used after initialization.
         # See: https://docs.djangoproject.com/en/dev/topics/http/middleware/#marking-middleware-as-unused
         raise MiddlewareNotUsed("Database check completed")
