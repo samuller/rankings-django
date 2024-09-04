b451ecb32a06a2d8982296c630b2b69d1d3615bd
Simon Muller <samullers@gmail.com>
2024-09-04 10:03:10 +0200

    Fix mypy issue with dynamic args to Jinja

--- rankings/rankings/jinja2.py
+++ rankings/rankings/jinja2.py
@@ -33,7 +33,7 @@ def url_for(endpoint: str, **values: Dict[str, Any]) -> str:
 def environment(**options: Dict[str, Any]) -> Environment:
     """Define Jinja environment configurations."""
     # TODO: enable autoescape - see: https://stackoverflow.com/questions/17257138/cant-disable-the-autoescape-in-jinja2
-    env = Environment(**options)  # noqa: S701
+    env = Environment(**options)  # type: ignore # noqa: S701
     env.globals.update(
         {
             "static": staticfiles_storage.url,
